d3951db92d75a510a3b436f4b625d8fb698b341e
Cadin Batrack <cadin@cadinbatrack.com>
2020-12-18 15:37:28 -0800

    adjust LCD shader threshold

--- playdate-env.lua
+++ playdate-env.lua
@@ -13,7 +13,7 @@ local deviceScale = 1.18
 lcdShader = love.graphics.newShader[[
 vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
 	vec4 pixel = Texel(texture, texture_coords );
-	if(pixel.r < 0.1) {
+	if(pixel.r < 0.65) {
 		return vec4(0.193, 0.184, 0.158, pixel.a);
 	} else {
 		return vec4(0.747, 0.757, 0.743, pixel.a);
